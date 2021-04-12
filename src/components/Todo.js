0cf51fb2d80adcc8dc4c023b8a920dc6e9298a2d
Rails <aluong343@gmail.com>
2021-04-12 23:26:00 +0700

    Add Tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -38,17 +38,35 @@ function Todo() {
       const newItem = {key: getKey(), text: text, done: false};
       putItems([...items,newItem]);
   };
+  const [tab,setTab] = useState("すべて");
+  const itemTab = () => {
+      const tabItem = items.filter((item) => {
+          if (tab === "すべて")
+              return item;
+          if (tab === "未完了" && !item.done) {
+              return item;
+          }
+          if (tab === "完了済み" && item.done) {
+              return item;
+          }
+      });
+      return tabItem;
+  };
+  const handleChangeTab = (target) =>{
+      setTab(target);
+  };
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd}/>
-      {items.map(item => (
+       <Filter onClick={handleChangeTab}/>
+      {itemTab().map(item => (
          <TodoItem item={item} key={item.key} onCheck={handleCheckTodoItem}/>
       ))}
       <div className="panel-block">
-        {items.length} items
+        {itemTab().length} items
       </div>
     </div>
   );
