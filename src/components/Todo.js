9494c06725c4b759d2c927ced0377ab36166e91a
Quang Anh <aluong343@gmail.com>
2021-05-21 09:26:02 +0700

    Save Todo on Firestore

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,11 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+/* コンポーネント */
+import TodoItem from './TodoItem';
+import Input from './Input';
+import Filter from './Filter';
+/* ライブラリ */
+import {getKey} from "../lib/util";
+import useStorageFirebase from "../hooks/storage_firebase";
 
 /* 
   【Todoのデータ構成】
@@ -7,69 +14,56 @@ import React, { useState } from 'react';
 　・done：完了状態（Boolean true:完了済み,, false:未完了）
 */
 
-/* コンポーネント */
-import TodoItem from './TodoItem';
-import Input from './Input';
-import Filter from './Filter';
-
 /* カスタムフック */
-import useStorage from '../hooks/storage';
-
-/* ライブラリ */
-import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems,cleanItems] = useStorage();
-  const handleCheckTodoItem = (i) => {
-      const newItems = items.map((item) => {
-          if(item.key === i.key)
-              item.done = !item.done;
-          return item;
-      });
-        putItems(newItems);
+    const [items, addItem, updateItem, cleanItems] = useStorageFirebase();
+
+    const handleCheckTodoItem = (item) => {
+        updateItem(item);
     };
-  const handleAdd = (text) => {
-      const newItem = {key: getKey(), text: text, done: false};
-      putItems([...items,newItem]);
-  };
-  const [tab,setTab] = useState("すべて");
-  const itemTab = () => {
-      const tabItem = items.filter((item) => {
-          if (tab === "すべて")
-              return item;
-          if (tab === "未完了" && !item.done) {
-              return item;
-          }
-          if (tab === "完了済み" && item.done) {
-              return item;
-          }
-      });
-      return tabItem;
-  };
-  const handleChangeTab = (target) =>{
-      setTab(target);
-  };
-  const handleCleanItem = () =>{
-    cleanItems();
-};
-  return (
-    <div className="panel">
-      <div className="panel-heading">
-        ITSS ToDoアプリ
-      </div>
-      <Input onAdd={handleAdd}/>
-       <Filter onClick={handleChangeTab}/>
-      {itemTab().map(item => (
-         <TodoItem item={item} key={item.key} onCheck={handleCheckTodoItem}/>
-      ))}
-        <div className="panel-block">
-             {itemTab().length} items
+    const handleAdd = (text) => {
+        const newItem = {key: getKey(), text: text, done: false};
+        addItem(newItem);
+    };
+    const [tab, setTab] = useState("すべて");
+    const itemTab = () => {
+        return items.filter((item) => {
+            if (tab === "すべて") {
+                return item;
+            }
+            if (tab === "未完了" && !item.done) {
+                return item;
+            }
+            if (tab === "完了済み" && item.done) {
+                return item;
+            }
+        });
+    };
+    const handleChangeTab = (target) => {
+        setTab(target);
+    };
+    const handleCleanItem = () => {
+        cleanItems();
+    };
+    return (
+        <div className="panel">
+            <div className="panel-heading">
+                ITSS ToDoアプリ
+            </div>
+            <Input onAdd={handleAdd}/>
+            <Filter onClick={handleChangeTab}/>
+            {itemTab().map(item => (
+                <TodoItem item={item} key={item.key} onCheck={handleCheckTodoItem}/>
+            ))}
+            <div className="panel-block">
+                {itemTab().length} items
+            </div>
+            <div className="panel-block">
+                <button className="button is-light is-fullwidth" onClick={handleCleanItem}>全てのToDoを削除</button>
+            </div>
         </div>
-        <div className="panel-block">
-            <button className="button is-light is-fullwidth" onClick={handleCleanItem}>全てのToDoを削除</button>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default Todo;
